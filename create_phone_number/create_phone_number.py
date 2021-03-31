c1ebb52037f09ca9f3e91111c7de9b694af89157
Pablo Arnau <pabloarnau@MacBook-Pro-de-Pablo.local>
2021-03-31 21:19:27 +0200

    Removed debug message

--- create_phone_number/create_phone_number.py
+++ create_phone_number/create_phone_number.py
@@ -9,6 +9,6 @@ def create_phone_number(number: List[int]):
     if not len(number) == MUST_HAVE_LENGHT:
         raise ValueError(f"Lenght of attribute number must be {MUST_HAVE_LENGHT}")
     if any([i < MIN or i > MAX for i in number]):
-        print(number)
         raise ValueError(f"All number should be in range ({MIN}, {MAX})")
+
     return "({}{}{}) {}{}{}-{}{}{}{}".format(*number)
